fd6db826025dbc5494200538338d5af9fc0ee7c0
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-08-22 08:44:52 +0530

    final commit

---
+++ src/FurnitureItem.java
@@ -0,0 +1,11 @@
+public class FurnitureItem {
+    String colour;
+    int grade;
+    String type;
+    String use;
+    int price;
+
+    if(use == "outdoor"){
+        price = price*0.95;
+    }
+}
